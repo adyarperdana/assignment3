866b3c2d9af4b76f7c2edf28f8bf8329471f7e8c
Adya Rahmat Perdana <adyarperdana@gmail.com>
2024-07-16 10:30:57 +0700

    1st commit

---
+++ exercise4-template.js
@@ -0,0 +1,35 @@
+/**
+Diberikan satu buah input berupa array a.
+Dari semua kemungkinan subarray yang berasal dari a, subarray manakah yang akan memberikan jumlah/sum terbesar apabila semua anggota subarray tersebut dijumlahkan?
+
+Input:
+a = [-2, -3, 4, -1, -2, 1, 5, -3]
+
+Expected Output:
+[[ 4, -1, -2, 1, 5] , 7]
+
+Explanation:
+Diantara semua kemungkinan subarray, [ 4, -1, -2, 1, 5]  merupakan subarray yang akan memberikan jumlah terbesar
+
+Nilai 7  merupakan jumlah atau sum dari subarray  [ 4, -1, -2, 1, 5]
+ */
+
+function subArray(arr) {
+    let nilaimax = 0;
+    let subarray = [];
+    for (let i = 0; i < arr.length; i++) {
+        let nilai = arr[i];
+        for (let j = i+1; j < arr.length; j++) {
+            nilai += arr[j];
+            if (nilaimax < nilai) {
+                nilaimax = nilai;
+                subarray = arr.slice(i,j+1);
+            }
+        }
+    }
+    return [subarray, nilaimax];
+}
+
+// Test Case
+let input = [-2, -3, 4, -1, -2, 1, 5, -3]
+console.log(subArray(input)) // output:[ [ 4, -1, -2, 1, 5 ], 7 ]
